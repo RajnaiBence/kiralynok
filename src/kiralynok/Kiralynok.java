432aea6f2f6c9d1d1d684f4609029b0f864e37a4
Rajnai Bence <Rajnai Bence>
2021-02-17 10:38:19 +0100

    OOP 6. feladatig

--- src/kiralynok/Kiralynok.java
+++ src/kiralynok/Kiralynok.java
@@ -4,15 +4,42 @@ import java.io.IOException;
 
 public class Kiralynok {
 
+    private Tabla tabla;
+
+    public Kiralynok() {
+        tabla  = new Tabla('#');
+        megoldasok();
+    }
+    
+    
+    
     public static void main(String[] args) throws IOException {
+        
+        Kiralynok kir = new Kiralynok();
+        kir.megoldasok();
+        
+    }
+
+    private void megoldasok() {
+       feladat4();
+       feladat6();
+      
+    }
+
+    private void feladat4() {
         Tabla tabla = new Tabla('#');
         System.out.println("4. feadat: Öres tábla");
         tabla.megjelenit();
         
+    }
+
+    private void feladat6() {
+            
         System.out.println(". feladat: Feltöltött megjelenitése");
         tabla.elhelyez(5);
         tabla.megjelenit();
-        
     }
 
+    
+    
 }
