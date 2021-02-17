137c4f9011a5317439be515e28915c12eb853eb0
Rajnai Bence <Rajnai Bence>
2021-02-17 10:29:53 +0100

    6.feladat kész

--- src/kiralynok/Kiralynok.java
+++ src/kiralynok/Kiralynok.java
@@ -6,9 +6,11 @@ public class Kiralynok {
 
     public static void main(String[] args) throws IOException {
         Tabla tabla = new Tabla('#');
-        
         System.out.println("4. feadat: Öres tábla");
+        tabla.megjelenit();
         
+        System.out.println(". feladat: Feltöltött megjelenitése");
+        tabla.elhelyez(5);
         tabla.megjelenit();
         
     }
